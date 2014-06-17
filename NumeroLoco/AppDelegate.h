fdd0ae82ed7f65ce4e941c2a5bf01ff757ec62e0
Diego Vidal <diefervidal91@gmail.com>
2014-06-17 08:38:53 -0500

    Initial Commit

---
+++ NumeroLoco/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NumeroLoco
+//
+//  Created by Developer on 17/06/14.
+//  Copyright (c) 2014 iAm Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NumeroLoco/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NumeroLoco
+//
+//  Created by Developer on 17/06/14.
+//  Copyright (c) 2014 iAm Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
